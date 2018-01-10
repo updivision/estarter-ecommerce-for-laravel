d0532013f2a90b2258915b9e3e08588929cb1b7d
Diana <diana.marusic@updivision.com>
2018-01-10 17:51:07 +0200

    modified integer migration according to Laravel 5 - bugfix

--- database/migrations/2017_07_03_000009_create_order_statuses_table.php
+++ database/migrations/2017_07_03_000009_create_order_statuses_table.php
@@ -17,7 +17,6 @@ class CreateOrderStatusesTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name', 50)->nullable()->default(null);
-            // $table->integer('notification', 1)->default(1);
             $table->integer('notification')->default(1);
         });
     }
