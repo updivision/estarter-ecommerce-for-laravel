637bd61a7ececd928a7dd93ba4cda61803612284
Diana <diana.marusic@updivision.com>
2018-01-10 14:06:48 +0200

    delete notifications table

--- database/migrations/2017_08_17_131600_create_specific_prices_table.php
+++ database/migrations/2017_08_17_131600_create_specific_prices_table.php
@@ -21,18 +21,12 @@ class CreateSpecificPricesTable extends Migration
             $table->dateTime('start_date');
             $table->dateTime('expiration_date');
             $table->integer('product_id')->unsigned()->nullable();
-            // $table->integer('currency_id')->unsigned()->nullable();
 
             // Foreign keys
             $table->foreign('product_id')
                 ->references('id')->on('products')
                 ->onDelete('no action')
                 ->onUpdate('no action');
-
-            // $table->foreign('currency_id')
-            //     ->references('id')->on('currencies')
-            //     ->onDelete('no action')
-            //     ->onUpdate('no action');
         });
     }
 
